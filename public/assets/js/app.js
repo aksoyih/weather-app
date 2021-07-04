a2ce09ef4f6acdcca55645ec5465a02532849375
haluk <i.halukaksoy@gmail.com>
2021-07-04 13:52:54 +0300

    heroku deployment prep

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,22 +1,21 @@
-const form = document.querySelector('form')
-const searchText = document.querySelector('input')
-const forecastDataShower = document.getElementById('forecastData')
+const form = document.querySelector("form");
+const searchText = document.querySelector("input");
+const forecastDataShower = document.getElementById("forecastData");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+  forecastDataShower.innerHTML = "Loading data...";
 
-    forecastDataShower.innerHTML = "Loading data...";
+  const location = searchText.value;
 
-    const location = searchText.value
-
-    fetch('/weather?adress='+encodeURI(location)).then((response) => {
-        response.json().then((data)=>{
-            if(data.error){
-                forecastDataShower.innerHTML = data.error
-            }else{
-                forecastDataShower.innerHTML = data.forecast
-            }
-        })
-    })
-})
+  fetch("/weather?adress=" + encodeURI(location)).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        forecastDataShower.innerHTML = data.error;
+      } else {
+        forecastDataShower.innerHTML = data.forecast;
+      }
+    });
+  });
+});
